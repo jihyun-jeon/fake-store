6685d3f806dec82cc4cdc114d98182972a91a295
jihyun-jeon <jhplus13@naver.com>
2024-12-21 01:49:46 +0900

    feat: react-query 설치 및 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import Router from '@/router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Router />
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
